dd54a47d39ec8a3342a572d6186569faac155482
Sari-E <63053303+Sari-E@users.noreply.github.com>
2020-11-05 19:37:41 +0200

    Update main.java

--- main.java
+++ main.java
@@ -3,11 +3,9 @@
  * semester 2 2020
  * 
  * Devora Sanhedrai 
- * ID: 211544614
  * mail: devora0556@gmail.com
  * 
  * Sari Eisikovits 
- * ID: 211849351 
  * mail: sari.eisikovits@gmail.com
  */
 import primitives.*;
