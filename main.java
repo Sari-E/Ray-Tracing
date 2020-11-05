8bdc9aca7487971fd81db943e1837a9702ea9d86
Sari-E <63053303+Sari-E@users.noreply.github.com>
2020-11-05 16:05:06 +0200

    Add files via upload

---
+++ main.java
@@ -0,0 +1,27 @@
+
+/*Mini Project OS
+ * semester 2 2020
+ * 
+ * Devora Sanhedrai 
+ * ID: 211544614
+ * mail: devora0556@gmail.com
+ * 
+ * Sari Eisikovits 
+ * ID: 211849351 
+ * mail: sari.eisikovits@gmail.com
+ */
+import primitives.*;
+
+
+public final class main {
+
+	/**
+	 * Main program to tests initial functionality of the 1st stage
+	 * 
+	 * @param args irrelevant here
+	 */
+	public static void main(String[] args) {
+
+		
+	}
+}
